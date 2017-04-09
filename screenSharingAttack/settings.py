fb2245902ee8d5f4aab0c648229d47ee375a8339
mike <280749941@qq.com>
2017-04-09 11:18:21 -0400

    database

--- screenSharingAttack/settings.py
+++ screenSharingAttack/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'screenSharingAttack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER': 'mike',
-        'PASSWORD': '21619646px',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
